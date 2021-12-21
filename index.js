775b846cdcc94e0bc00ac622312f51a9239d4477
Christopher Terrano <mterrano1@yahoo.com>
2021-12-21 09:42:38 -0600

    Done.

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
+// create a function that takes 2 arguments: an array of numbers, and a number. 
+// the function should return true if any of the two numbers in the array equal the number in the argument after adding them together.
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  // Iterate through the array and add the first number with every other number
+      for (let i = 0; i < array.length; i++){
+          let x = target - array[i]
+          for (let j = i + 1; j < array.length; j++){
+              if (array[j] === x){
+                  return true
+              }
+          }
+      }
+   return false
 }
 
 /* 
   Write the Big O time complexity of your function here
+  0(n^2)
 */
 
 /* 
   Add your pseudocode here
+
+  Iterate through the array and add the first number with every other number
+  Return true if the sum of the numbers equals the target number
+  Repeat this process with every number in the array
+  If none of the sums equals the target number, return false
+
 */
 
 /*
   Add written explanation of your solution here
+
+
+
 */
 
 // You can run `node index.js` to view these console logs
